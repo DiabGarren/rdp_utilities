7054395f14156720c89800ee060a7e27565ed6ed
DiabGarren <garrendiab@gmail.com>
2023-08-17 15:13:50 +0200

    Login and dashboard barebone

---
+++ components/header.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import Link from "next/link";
+import ProfileIcon from "./profileIcon";
+
+export default function Header({ pageName }: any) {
+    let userId = localStorage.getItem("userId");
+    
+    if (!userId) {
+        return (
+            <header
+            className="
+        grid
+        h-[60px] md:h-[80px]
+        grid-cols-small-header md:grid-cols-big-header
+        bg-gradient-to-r from-blue-dark to-blue-light"
+        >
+            <Link
+                className="group
+                flex
+                items-center
+                justify-center
+                h-[60px] md:h-[100%]"
+                href="/"
+            >
+                <svg
+                    className="
+                    p-[10px] 
+                    h-[50px] 
+                    rounded-[50%] 
+                    group-hover:bg-blue-light"
+                    width="50"
+                    height="45"
+                    viewBox="0 0 50 45"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        className="group-hover:stroke-blue-dark"
+                        d="M10 16V43H25H40V16M2 15L25 2L48 15"
+                        stroke="white"
+                        strokeWidth="3"
+                        strokeLinecap="round"
+                    />
+                </svg>
+            </Link>
+            <h1
+                className="
+            flex flex-col
+            justify-center
+            text-white
+            text-[35px] md:text-[50px]
+            font-[700]"
+            >
+                {pageName}
+            </h1>
+        </header>
+        )
+    }
+
+    return (
+        <header
+            className="
+        grid
+        h-[60px] md:h-[80px]
+        grid-cols-small-header md:grid-cols-big-header
+        bg-gradient-to-r from-blue-dark to-blue-light"
+        >
+            <Link
+                className="group
+                flex
+                items-center
+                justify-center
+                h-[60px] md:h-[100%]"
+                href="/"
+            >
+                <svg
+                    className="
+                    p-[10px] 
+                    h-[50px] 
+                    rounded-[50%] 
+                    group-hover:bg-blue-light"
+                    width="50"
+                    height="45"
+                    viewBox="0 0 50 45"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        className="group-hover:stroke-blue-dark"
+                        d="M10 16V43H25H40V16M2 15L25 2L48 15"
+                        stroke="white"
+                        strokeWidth="3"
+                        strokeLinecap="round"
+                    />
+                </svg>
+            </Link>
+            <h1
+                className="
+            flex flex-col
+            justify-center
+            text-white
+            text-[35px] md:text-[50px]
+            font-[700]"
+            >
+                {pageName}
+            </h1>
+            <ProfileIcon userId={userId} />
+        </header>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            white: '#ffffff',
+            blue: {
+                light: '#bfdee8',
+                dark: '#00799f',
+            },
+            red: {
+                light: '#ff9393',
+                dark: '#910505',
+            },
+            green: {
+                light: '#50f650',
+                dark: '#028202',
+            },
+            grey: {
+                light: '#cccccc',
+                dark: '#707070',
+            },
+        },
+
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            gridTemplateColumns: {
+                'small-header': '50px 1fr 50px',
+                'big-header': '80px 1fr 65px',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
